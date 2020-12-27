cf41dc2bab6216608f7c8207eb20deee13654632
Ernest Salim <Ernest@Ernests-MacBook-Pro-5.local>
2020-12-27 23:57:20 +0700

    first commit

---
+++ post.js
@@ -0,0 +1,48 @@
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+// mobiscroll.datepicker('#mobi-picker', {
+//     controls: ['date'],
+//     touchUi: true
+// });
+
+
+// window.alert("working")
+
+// mobiscroll.setOptions({
+//     theme: 'ios',
+//     themeVariant: 'light'
+// });
+
+// mobiscroll.datepicker('#demo-mobile-picker-input', {
+//     controls: ['date']
+// });
+
+// var instance = mobiscroll.datepicker('#demo-mobile-picker-button', {
+//     controls: ['date'],
+//     showOnClick: false,
+//     showOnFocus: false,
+// });
+
+// instance.setVal(new Date(), true);
+
+// mobiscroll.datepicker('#demo-mobile-picker-mobiscroll', {
+//     controls: ['date']
+// });
+
+// mobiscroll.datepicker('#demo-mobile-picker-inline', {
+//     controls: ['date'],
+//     display: 'inline'
+// });
+
+// document
+//     .getElementById('show-mobile-date-picker')
+//     .addEventListener('click', function () {
+//         instance.open();
+//         return false;
+//     });
+
